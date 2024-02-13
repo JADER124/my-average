f5d6e7f28e5814de5d3e6592bf05a8ac27522b91
Jhon Jader López <jaderlopez186@gmail.com>
2024-02-13 09:16:25 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
     </div>
   );
 }
