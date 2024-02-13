c19ea3cea4b5039f1daade8d5598683b6da6c80a
juanmax <juan.gonzalez69@correo.tdea.edu.co>
2024-02-13 10:21:57 -0500

    Segundo Commmit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h1 className="text-3xl font-bold underline">Hello worlddddd!</h1>
+      <h1 className="text-3xl font-bold underline">Hello worldddddddddddddddddddxdxd!</h1>
     </div>
   );
 }
