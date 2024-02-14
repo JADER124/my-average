91eef6312e787aaac8a9a1bed6f29496bce2b7c0
juanmax <juan.gonzalez69@correo.tdea.edu.co>
2024-02-14 09:36:15 -0500

    Install material tailwind

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Button } from "@material-tailwind/react";
 const home = () => {
   return (
     <div>
       <h1>hola mundo</h1>
+        <Button>Button</Button>;
     </div>
   );
 };
