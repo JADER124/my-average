9a36720d01a20809e4afbc26a2eb933058cac2c5
juanmax <juan.gonzalez69@correo.tdea.edu.co>
2024-02-19 09:53:14 -0500

    test

--- src/components/pages/nav.js
+++ src/components/pages/nav.js
@@ -48,7 +48,7 @@ const Nav = () => {
               href="/"
               className="mr-4 cursor-pointer p-1.5 font-medium"
             >
-              MyAverage
+              MyAveragee
             </Typography>
           </div>
           <div className="flex items-center gap-4">
