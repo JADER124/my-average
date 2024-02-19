4f52e364a744adddf9dfdb01ae31632e412a3ae7
juanmax <juan.gonzalez69@correo.tdea.edu.co>
2024-02-19 10:31:20 -0500

    Table

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "@material-tailwind/react";
+import { Button,Input } from "@material-tailwind/react";
 
 const home = () => {
   return (
@@ -7,33 +7,22 @@ const home = () => {
       <table className="min-w-full divide-y divide-gray-200">
         <thead>
           <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Nameeeee
+            <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Nota
             </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Emaillll
+            <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Porcentaje
             </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Role
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Status
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider">
               Action
             </th>
           </tr>
         </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
+        <tbody className="">
           <tr>
-            <td className="px-6 py-4 whitespace-nowrap">Jane Doe</td>
-            <td className="px-6 py-4 whitespace-nowrap">jane@example.com</td>
-            <td className="px-6 py-4 whitespace-nowrap">Admin</td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                Active
-              </span>
-            </td>
+            <td className="px-6 py-4 whitespace-nowrap"><Input className="w-48" placeholder="Nota" /></td>
+            <td className="px-6 py-4 whitespace-nowrap"><Input label="Username" /></td>
+
             <td className="px-6 py-4 whitespace-nowrap">
               <button className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">
                 Edit
@@ -43,24 +32,7 @@ const home = () => {
               </button>
             </td>
           </tr>
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">John Doe</td>
-            <td className="px-6 py-4 whitespace-nowrap">john@example.com</td>
-            <td className="px-6 py-4 whitespace-nowrap">User</td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
-                Inactive
-              </span>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <button className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">
-                Edit
-              </button>
-              <button className="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">
-                Delete
-              </button>
-            </td>
-          </tr>
+         
         </tbody>
       </table>
     </div>
