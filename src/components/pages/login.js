ab8f2f7be7c5427c9c8b5939f44956a90846f0a2
Jhon Jader López <jaderlopez186@gmail.com>
2024-03-18 10:05:18 -0500

    auth firebase

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,28 +1,25 @@
-import React,{useEffect,useState} from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/calcular.png";
 import { FaArrowLeft } from "react-icons/fa";
-import { collection,getDocs ,query,where} from "firebase/firestore";
-import {db} from "../../firebase/config"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/config";
 
 const Login = () => {
-
-
-
-  useEffect(()=>{
-    const dataRef= collection(db, "data");
-    const q = query(dataRef,where("email","==","jua@gmail.com"));
-    console.log(q.docs)
-    getDocs(q)
-      .then((resp)=>{
-        console.log(
-          resp.docs.map((doc)=>{
-            return {...doc.data(),id:doc.id}
-          })
-
-        )
-      })
-  },[])
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential.user.uid);
+    } catch (error) {
+      alert(e.code);
+    }
+  };
   return (
     <div>
       <div className="absolute border-2 border-gray-400/50 rounded-xl mt-5 ml-20 hover:bg-cyan-600/30">
@@ -54,6 +51,9 @@ const Login = () => {
                   type="email"
                   autoComplete="email"
                   required
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -83,6 +83,9 @@ const Login = () => {
                   type="password"
                   autoComplete="current-password"
                   required
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -91,6 +94,7 @@ const Login = () => {
             <div>
               <button
                 type="submit"
+                onClick={(e) => login(e)}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
--- src/components/pages/signIn.js
+++ src/components/pages/signIn.js
@@ -2,17 +2,18 @@ import React from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import logo from "../../imgs/calcular.png";
 import { useFormik } from "formik";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/config";
 import * as Yup from "yup";
 export default function SignIn() {
+  
   const formik = useFormik({
     initialValues: {
       name: "",
       email: "",
       password: "",
       password2: "",
-      materias:[
-        
-      ]
+      materias: [],
     },
     validationSchema: Yup.object({
       name: Yup.string()
@@ -33,18 +34,24 @@ export default function SignIn() {
     }),
 
     onSubmit: (values) => {
-      console.log(values);
-
-
+      register(values.email, values.password);
     },
   });
+  const register = async (email, password) => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(user)
+    } catch (e) {
+      alert(e.code);
+    }
+  };
   return (
     <>
-    <div className="absolute border-2 border-gray-400/50 rounded-xl mt-5 ml-20 hover:bg-cyan-600/30">
-      <a href="/">
-      <FaArrowLeft className="text-3xl text-gray-400"/>
-      </a>
-    </div>
+      <div className="absolute border-2 border-gray-400/50 rounded-xl mt-5 ml-20 hover:bg-cyan-600/30">
+        <a href="/">
+          <FaArrowLeft className="text-3xl text-gray-400" />
+        </a>
+      </div>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img className="mx-auto h-20 w-20 " src={logo} alt="Your Company" />
