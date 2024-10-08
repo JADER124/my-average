e47bfe2b5ab1a14c8872ff36f9d640dd3436f1a6
JADER124 <jaderlopez186@gmail.com>
2024-10-08 10:49:11 -0500

    delete footer

--- src/components/pages/footer.js
+++ src/components/pages/footer.js
@@ -3,11 +3,9 @@ import { Typography } from "@material-tailwind/react";
 
 function Footer() {
   return (
-    <footer className="w-full align-bottom bg-gray-200 p-4 mt-4">
+    <footer className="w-full align-bottom bg-gray-200 p-8 mt-4">
       <Typography color="blue-gray" className="text-center font-normal">
-        &copy; 2024 MyAverage Desarrollado por: <br />
-        <a rel="noreferrer" target="_blank" href="https://github.com/JuanManuel0909" className="hover:text-blue-500 underline">Juan Gonzalez</a> y{" "}
-        <a rel="noreferrer" target="_blank" href="https://github.com/JADER124" className="hover:text-green-400 underline">Jader Lopez</a>
+        &copy; 2024 MyAverage 
       </Typography>
     </footer>
   );
